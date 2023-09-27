f1ec8563b4650fb8011a0d4c303a5cc2c537ae73
muneeb syed <imuneebsyed@gmail.com>
2023-09-27 14:54:57 +0530

    connect to database

--- api/index.js
+++ api/index.js
@@ -1,6 +1,19 @@
 import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+mongoose
+  .connect(process.env.MONGO)
+  .then(() => {
+    console.log("connected to db");
+  })
+  .catch(() => {
+    console.log(err);
+  });
 const app = express();
 
-app.listen(3000,()=>{
-    console.log("server is running....????")
-})
+app.listen(3000, () => {
+  console.log("server is running....????");
+});
